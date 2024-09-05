d50ed153752ce4759a9546e784ab798b252e05e7
TahaHachana <tahahachana@gmail.com>
2024-09-05 05:39:04 +0100

    nato output

---
+++ nato-output/word_parse01.c
@@ -0,0 +1,22 @@
+// gcc -Wall -std=c11 -pedantic word_parse01.c -o word && ./word
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char sometext[64];
+    char *match;
+
+    printf("Type some text: ");
+    fgets(sometext, 64, stdin);
+
+    match = strtok(sometext, " ");
+    while (match)
+    {
+        printf("%s\n", match);
+        match = strtok(NULL, " ");
+    }
+
+    return (0);
+}
